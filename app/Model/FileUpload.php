1dd72503293b9a97d8ca0f237c456c673fbaa69c
paul <paul_0500585024@yahoo.com>
2020-09-20 22:42:50 +0700

    fixing no 4

--- app/Model/FileUpload.php
+++ app/Model/FileUpload.php
@@ -1,5 +1,24 @@
 <?php
 
 class FileUpload extends AppModel {
-
+    public $validate = array(
+        'file' => array(
+            'rule' => array('mimeType', array('text/csv','text/plain')),
+            'message' => "Invalid File CSV"
+        ),
+        'name' => array(
+            'rule' => array('maxLength', 50),
+            'message' => "Name must be no larger than 50 characters long"
+        ),
+        'email' => array(
+            'maxlength' => array(
+                'rule' => array('maxLength', 250),
+                'message' => "Email must be no larger than 50 characters long"
+            ),
+            'isUnique' => array(
+                'rule' => array('isUnique', array('email', 'name'), false),
+                'message' => "This name & email combination has already been used."
+            )
+        ),
+    );
 }
